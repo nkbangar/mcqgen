0e488a6149807485b1f9411c5c92d785efa3e7bc
Nand Kishore Bangar <nkbangar.c@stc.com.sa>
2025-03-13 15:34:37 +0300

    file updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
 # To install local package for virtual environment
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenrator',
+    version='0.0.1',
+    author='NK Bangar',
+    author_email='nand.bangar@gmail.com',
+    install_requires=["openai","langchain","streamlit","python-dotenv","PyPDF2"],
+    packages=find_packages()
+)
